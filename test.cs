738f2315246038d1fde71c7e9f2bff86c39c8bc3
なすの <160385705+NasuNo012@users.noreply.github.com>
2025-05-23 22:42:34 +0900

    xx を変更 (#3)

--- test.cs
+++ test.cs
@@ -3,5 +3,13 @@ private void Main(string[] args)
     var num1 = Math.Rand() * 10;
     var num2 = Math.Rand() * 10;
 
+    // ほげほげ
     Console.WriteLine($"{num1} + {num2} = {num1 + num2}")
+
+
+    aaaaaaaaaaaaa
+    aaaaaaaaaaaaa
+
+    aaaaaaaaaaaaaa
+    aaaaaaaaaaaaaa
 }
